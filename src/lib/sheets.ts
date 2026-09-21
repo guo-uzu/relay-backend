7ab3142e010c04221581512da44d12af67bdab6c
Alejandro Camacho <alejandro@uzu.digital>
2026-09-21 16:58:28 -0600

    feat: format data in routes.sheet.ts
    feat: format data in routes.sheet.ts

--- src/lib/sheets.ts
+++ src/lib/sheets.ts
@@ -1,15 +1,11 @@
-
-import { google } from "googleapis"
+import { google } from "googleapis";
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: "./relay-509018-0e6dd68f6bad.json",
-  scopes: [
-    "https://www.googleapis.com/auth/spreadsheets",
-  ]
-})
+  keyFile: "./relay-509018-2a24d38ac416.json",
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
 
 export const sheets = google.sheets({
   version: "v4",
-  auth
-})
-
+  auth,
+});
